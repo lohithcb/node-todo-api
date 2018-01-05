469d920575b46fe04aaa9483c44dd8a09368fbb5
lohithcb <lohith@inventbird.com>
2018-01-05 14:58:07 +0530

    add tests for GET /todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,9 +4,17 @@ const request = require('supertest');
 var {app} = require('./../server');
 var {Todo} = require('./../models/todo');
 
+const todos = [{
+    text: 'this is first todo'
+}, {
+    text: 'this is second todo'
+}];
+
 //this code is run before our test case starts
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -25,7 +33,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -44,9 +52,21 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find().then((res) => {
-                    expect(res.length).toBe(0);
+                    expect(res.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             })
+    });
+});
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done());
     });
 });
